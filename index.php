dfd0bba2359c6bdd2b096faedcfaff80d472ed21
nittingit <63450715+nittingit@users.noreply.github.com>
2020-08-20 15:55:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+echo "Hello from version1.1 UNNNATI for IBM GUYS ";
+echo "S2I in openshift";
+?>
+
+</body>
+</html>
